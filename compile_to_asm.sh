88ed013bff2a9170b0016b29e370e9602334f2db
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-10-03 15:37:27 +0200

    Added option for gcc for preprocessed  sourcefiles

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -2,7 +2,7 @@
 # $2 output path
 opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
 
-if ! gcc -DHAVE_CONFIG_H -I. -I./config -I. -I. -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32 -I/usr/include/libxml2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -fopenmp -Wall -g -mtune=amdfam10 -fexceptions -pthread -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32 -c "$1" -o "$2" -S -masm=intel $opt; then
+if ! gcc -x cpp-output -DHAVE_CONFIG_H -I. -I./config -I. -I. -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32 -I/usr/include/libxml2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -fopenmp -Wall -g -mtune=amdfam10 -fexceptions -pthread -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32 -c "$1" -o "$2" -S -masm=intel $opt; then
 	echo "error"
 	exit 1
 fi
